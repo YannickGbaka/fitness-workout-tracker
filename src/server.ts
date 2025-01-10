04e03a85082cd991a97a086dcb9d63df94478804
Yannick <gosseyannick553@gmail.com>
2025-01-10 12:28:53 +0000

    connect mongo db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import app from './index.ts'
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
 
 // Load environment variables
 config();
 
 const PORT = process.env.PORT || 3006;
+
+mongoose
+  .connect(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
