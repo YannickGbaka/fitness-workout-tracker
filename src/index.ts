9e47c1e659e090b4d8c96006cdb44c827b0759ad
Yannick <gosseyannick553@gmail.com>
2025-01-14 15:31:49 +0000

    feat: add rate limiter to the auth endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import {errorHandler, errorConverter} from './middlewares/error.middleware.ts';
 import ApiError from './utils/apiErrors.util.ts';
 import status from 'http-status';
+import authLimiter from './middlewares/rateLimiter.middleware.ts';
 import('./strategies/jwt-strategy.ts');
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(helmet());
 app.get('/', (request, response)=>{
     return response.status(200).json({message: 'Hello world'});
 })
-app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', authLimiter, authRouter);
 // handle error
 // send back a 404 error for any unknown api request
 
